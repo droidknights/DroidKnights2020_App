91602800326052c9c5fda00e17506e9cebefe2d3
z0 <ji9310@naver.com>
2019-12-23 17:30:21 +0900

    ci 테스트

--- androidapp/app/src/main/java/com/droidknights/app2020/di/DKViewModelFactory.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/di/DKViewModelFactory.kt
@@ -11,8 +11,7 @@ import javax.inject.Provider
 class DKViewModelFactory @Inject constructor(private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         val found = creators.entries.find { modelClass.isAssignableFrom(it.key) }
-        val creator = found?.value
-            ?: throw IllegalArgumentException("unknown model class $modelClass")
+        val creator = found?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
         try {
             @Suppress("UNCHECKED_CAST")
             return creator.get() as T
