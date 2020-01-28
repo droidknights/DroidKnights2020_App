abdd22f0dae2eb7ef98a90a813d510a0ec518865
pluulove <pluulove@gmail.com>
2020-01-29 02:04:01 +0900

    Dagger 사용 수정
    - Interface 정리

--- androidapp/app/src/main/java/com/droidknights/app2020/base/DispatcherProvider.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/base/DispatcherProvider.kt
@@ -2,14 +2,18 @@ package com.droidknights.app2020.base
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
 interface DispatcherProvider {
-
-    fun main(): CoroutineDispatcher = Dispatchers.Main
-    fun default(): CoroutineDispatcher = Dispatchers.Default
-    fun io(): CoroutineDispatcher = Dispatchers.IO
-    fun unconfined(): CoroutineDispatcher = Dispatchers.Unconfined
-
+    fun main(): CoroutineDispatcher
+    fun default(): CoroutineDispatcher
+    fun io(): CoroutineDispatcher
+    fun unconfined(): CoroutineDispatcher
 }
 
-class DefaultDispatcherProvider : DispatcherProvider
+class DefaultDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override fun main(): CoroutineDispatcher = Dispatchers.Main
+    override fun default(): CoroutineDispatcher = Dispatchers.Default
+    override fun io(): CoroutineDispatcher = Dispatchers.IO
+    override fun unconfined(): CoroutineDispatcher = Dispatchers.Unconfined
+}
--- androidapp/app/src/main/java/com/droidknights/app2020/di/AppModule.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/di/AppModule.kt
@@ -1,24 +1,15 @@
 package com.droidknights.app2020.di
 
-import android.content.Context
-import com.droidknights.app2020.MainApplication
 import com.droidknights.app2020.base.DefaultDispatcherProvider
 import com.droidknights.app2020.base.DispatcherProvider
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
 /**
  * Created by jiyoung on 29/11/2019
  */
 @Module
-class AppModule {
-    @Provides
-    fun provideContext(application: MainApplication): Context {
-        return application.applicationContext
-    }
-
-    @Provides
-    fun provideDispatchers(): DispatcherProvider {
-        return DefaultDispatcherProvider()
-    }
+internal interface AppModule {
+    @Binds
+    fun bindDispatchers(dispatcher: DefaultDispatcherProvider): DispatcherProvider
 }
