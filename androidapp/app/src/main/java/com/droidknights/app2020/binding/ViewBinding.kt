fa6bd7ee4882e10a7997842a42c5a5f9b9cb48c1
dot2line <yands11@gmail.com>
2020-02-18 00:08:03 +0900

    BindingAdapter를 한 파일에서 관리할 수 있도록 코드 이동

--- androidapp/app/src/main/java/com/droidknights/app2020/binding/ViewBinding.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/binding/ViewBinding.kt
@@ -2,6 +2,16 @@ package com.droidknights.app2020.binding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 @BindingAdapter(value = ["bindImgRes"])
-fun bindSetImage(imgView: ImageView, resId: Int) = imgView.setImageResource(resId)
+fun ImageView.bindSetImage(resId: Int) = setImageResource(resId)
+
+@BindingAdapter(value = ["isRefreshing"])
+fun SwipeRefreshLayout.bindRefreshing(isRefreshing: Boolean) {
+    this.isRefreshing = isRefreshing
+}
+
+@BindingAdapter(value = ["onRefresh"])
+fun SwipeRefreshLayout.bindRefreshListener(onRefreshListener: SwipeRefreshLayout.OnRefreshListener) =
+    setOnRefreshListener(onRefreshListener)
