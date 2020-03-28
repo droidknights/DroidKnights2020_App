85b1c6cd1b8c921e33ff5595f10c164ce3004f79
Taesung.Kim <taesung.kim@flitto.com>
2020-03-28 13:18:29 +0900

    fix: add dagger module

--- androidapp/app/src/main/java/com/droidknights/app2020/di/ActivityBindingModule.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/di/ActivityBindingModule.kt
@@ -4,6 +4,7 @@ import com.droidknights.app2020.ui.MainActivity
 import com.droidknights.app2020.di.annotation.ActivityScoped
 import com.droidknights.app2020.ui.info.InfoModule
 import com.droidknights.app2020.ui.MainActivityModule
+import com.droidknights.app2020.ui.info.sponsor.SponsorModule
 import com.droidknights.app2020.ui.mypage.MypageModule
 import com.droidknights.app2020.ui.schedule.ScheduleModule
 import dagger.Module
@@ -20,8 +21,9 @@ abstract class ActivityBindingModule {
             MainActivityModule::class,
             ScheduleModule::class,
             InfoModule::class,
-            MypageModule::class
+            MypageModule::class,
+            SponsorModule::class
         ]
     )
-    internal abstract fun getMainActivity() : MainActivity
+    internal abstract fun getMainActivity(): MainActivity
 }
