9b353226c5a7671f83239db9e9557481917530b2
improve777 <improve777@gmail.com>
2020-08-22 21:21:14 +0900

    브라우저를 띄우기 위해 videoEvent, qnaEvent 추가
    - videoLink 가 없거나 빈값일 경우 기본 URL 을 전달함
    - qnaLink 가 없거나 빈값일 경우 토스트 이벤트를 전달함

--- androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/detail/SessionDetailViewModel.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/detail/SessionDetailViewModel.kt
@@ -4,9 +4,11 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.droidknights.app2020.base.BaseViewModel
 import com.droidknights.app2020.base.DispatcherProvider
+import com.droidknights.app2020.common.Event
 import com.droidknights.app2020.data.Session
 import com.droidknights.app2020.db.SessionRepository
 import kotlinx.coroutines.flow.flowOn
+import com.droidknights.app2020.R
 
 class SessionDetailViewModel @ViewModelInject constructor(
     private val dispatchers: DispatcherProvider,
@@ -14,6 +16,15 @@ class SessionDetailViewModel @ViewModelInject constructor(
 ) : BaseViewModel() {
 
     private val _id: MutableLiveData<String> = MutableLiveData()
+
+    private val _videoEvent = MutableLiveData<Event<String>>()
+    val videoEvent: LiveData<Event<String>> = _videoEvent
+
+    private val _qnaEvent = MutableLiveData<Event<String>>()
+    val qnaEvent: LiveData<Event<String>> = _qnaEvent
+
+    private val _toastEvent = MutableLiveData<Event<Int>>()
+    val toastEvent: LiveData<Event<Int>> = _toastEvent
 
     val sessionContents: LiveData<Session> = _id.switchMap { id ->
         liveData {
@@ -25,5 +36,26 @@ class SessionDetailViewModel @ViewModelInject constructor(
 
     fun getSession(id: String) {
         _id.value = id
+    }
+
+    fun onClickVideoLink() {
+        val videoLink = sessionContents.value?.videoLink
+        _videoEvent.value = Event(
+            if (videoLink.isNullOrEmpty()) DEFAULT_VIDEO_LINK else videoLink
+        )
+    }
+
+    fun onClickQnALink() {
+        val qnaLink = sessionContents.value?.qnaLink
+        if (qnaLink.isNullOrEmpty()) {
+            _toastEvent.value = Event(R.string.session_detail_qna_link_tbd)
+            return
+        }
+
+        _qnaEvent.value = Event(qnaLink)
+    }
+
+    companion object {
+        private const val DEFAULT_VIDEO_LINK = "https://www.youtube.com/channel/UCjeUnwS8mHhsl600-nFJKmw"
     }
 }
