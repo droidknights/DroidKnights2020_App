bbae437c474733ee007f9a82b811706c30d3b21c
song <0603yang@naver.com>
2020-08-26 19:20:11 +0900

    feat : 세션 리스트 아이템에 해당 tag 달기

--- androidapp/app/src/main/java/com/droidknights/app2020/binding/ViewBinding.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/binding/ViewBinding.kt
@@ -2,8 +2,14 @@ package com.droidknights.app2020.binding
 
 import android.webkit.WebView
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.droidknights.app2020.R
+import com.droidknights.app2020.data.Tag
+import com.droidknights.app2020.ui.schedule.TagAdapter
+import com.droidknights.app2020.widget.SessionChip
 
 @BindingAdapter(value = ["bindImgRes"])
 fun ImageView.bindSetImage(resId: Int) = setImageResource(resId)
@@ -19,3 +25,22 @@ fun SwipeRefreshLayout.bindRefreshListener(onRefreshListener: SwipeRefreshLayout
 
 @BindingAdapter("webUrl")
 fun WebView.bindUrl(value: String?) = value?.let(::loadUrl)
+
+@BindingAdapter("setColor")
+fun SessionChip.setColor(color: Int) {
+    this.chipTextColor = color
+    this.strokeColor = color
+    this.chipBackgroundColor = when (color) {
+        ContextCompat.getColor(context, R.color.toolbar_textColor_black) -> ContextCompat.getColor(context, R.color.color_sessionChipText_white)
+        ContextCompat.getColor(context, R.color.color_sessionChipText_white) -> ContextCompat.getColor(context, R.color.toolbar_textColor_black)
+        else -> lighten(color, 30)
+    }
+}
+
+@BindingAdapter("setData")
+fun RecyclerView.setData(items: List<Tag>?) {
+    this.adapter = TagAdapter()
+    (this.adapter as TagAdapter).run {
+        submitList(items)
+    }
+}
--- androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/TagAdapter.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/TagAdapter.kt
@@ -1,4 +1,22 @@
 package com.droidknights.app2020.ui.schedule
 
-class TagAdapter {
+import androidx.recyclerview.widget.DiffUtil
+import com.droidknights.app2020.R
+import com.droidknights.app2020.common.DataBindingAdapter
+import com.droidknights.app2020.data.Tag
+
+class TagAdapter : DataBindingAdapter<Tag>(DiffCallback()) {
+    override var itemClickListener: ItemClickListener? = null
+
+    class DiffCallback : DiffUtil.ItemCallback<Tag>() {
+        override fun areItemsTheSame(oldItem: Tag, newItem: Tag): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: Tag, newItem: Tag): Boolean {
+            return oldItem.name == newItem.name
+        }
+    }
+
+    override fun getItemViewType(position: Int) = R.layout.item_session_tag
 }
