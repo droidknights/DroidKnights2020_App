af9edb657f81f401f9a189cdec955353f800bb29
improve777 <improve777@gmail.com>
2020-09-02 20:53:56 +0900

    연사자가 여러명일 때 프로필 이미지를 겹쳐서 표시

--- androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/detail/SessionDetailSpeakerView.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/ui/schedule/detail/SessionDetailSpeakerView.kt
@@ -1,7 +1,9 @@
 package com.droidknights.app2020.ui.schedule.detail
 
 import android.content.Context
+import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import com.bumptech.glide.Glide
@@ -9,6 +11,10 @@ import com.bumptech.glide.request.RequestOptions
 import com.droidknights.app2020.R
 import com.droidknights.app2020.data.Speaker
 import com.droidknights.app2020.databinding.ViewSessionDetailSpeakerBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SessionDetailSpeakerView @JvmOverloads constructor(
     context: Context,
@@ -18,6 +24,11 @@ class SessionDetailSpeakerView @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
 
     private lateinit var binding: ViewSessionDetailSpeakerBinding
+
+    private val profileSize =
+        context.resources.getDimensionPixelSize(R.dimen.session_detail_speaker_profile_size)
+    private val profileMargin = context.dpToPx(2)
+    private val profileOverlay = context.dpToPx(10)
 
     var speakers: List<Speaker> = emptyList()
         set(value) {
@@ -39,26 +50,133 @@ class SessionDetailSpeakerView @JvmOverloads constructor(
     }
 
     private fun fetchSpeakersImage(speakerProfileImages: List<String>) {
-        if (speakerProfileImages.isNotEmpty()) {
-            val profileSize =
-                context.resources.getDimensionPixelSize(R.dimen.session_detail_speaker_profile_size)
+        if (speakerProfileImages.isEmpty()) return
 
+        if (speakerProfileImages.size == 1) {
             Glide.with(this)
                 .load(speakerProfileImages[0])
                 .apply(
                     RequestOptions()
                         .override(profileSize)
-                        .placeholder(R.drawable.img_droid_space)
                         .error(R.drawable.img_droid_space)
                         .circleCrop()
                 )
                 .into(binding.ivSpeakerImage)
+            return
         }
 
-        // TODO : 프로필 이미지 여러개 표시
+        CoroutineScope(Dispatchers.Main).launch {
+            val bitmaps = getProfileBitmaps(speakerProfileImages)
+            val result = mergeProfiles(bitmaps)
+            binding.ivSpeakerImage.setImageBitmap(result)
+        }
     }
 
     private fun fetchSpeakersBelongAndName(speakers: List<Speaker>) {
         binding.tvSpeakerName.text = speakers.joinToString(" · ") { "${it.belong ?: ""} ${it.name}" }
     }
+
+    private suspend fun getProfileBitmaps(speakerProfileImages: List<String>): List<Bitmap> {
+        return withContext(Dispatchers.IO) {
+            val bitmaps = mutableListOf<Bitmap>()
+
+            speakerProfileImages.map { url ->
+                Glide.with(this@SessionDetailSpeakerView)
+                    .asBitmap()
+                    .load(url)
+                    .apply(
+                        RequestOptions()
+                            .override(profileSize)
+                            .error(R.drawable.img_droid_space)
+                            .circleCrop()
+                    )
+                    .submit()
+            }.forEachIndexed { i, future ->
+                val resource = future.get()
+
+                val isLastProfile = i == speakers.size - 1
+                if (isLastProfile) {
+                    bitmaps.add(resource)
+                } else {
+                    val mask = createCrescentMask(profileSize, profileSize)
+                    bitmaps.add(getClippedResource(resource, mask))
+                }
+            }
+            return@withContext bitmaps
+        }
+    }
+
+
+    private fun mergeProfiles(bitmaps: List<Bitmap>): Bitmap {
+        val totalWidth = (profileSize + profileMargin - profileOverlay) * (bitmaps.size - 1) + profileSize
+
+        val result = Bitmap.createBitmap(
+            totalWidth,
+            profileSize,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(result)
+
+        for (i in bitmaps.indices) {
+            canvas.drawBitmap(
+                bitmaps[i],
+                0 + ((profileSize - profileOverlay) * i).toFloat(),
+                0f,
+                null
+            )
+        }
+        return result
+    }
+
+    private fun createCrescentMask(width: Int, height: Int): Bitmap {
+        val mask = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(mask)
+
+        canvas.clipRect(0, 0, width, height)
+        canvas.drawColor(Color.BLACK)
+
+        val paint = Paint().apply {
+            color = Color.TRANSPARENT
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        }
+        val widthHalf = width / 2
+        val heightHalf = height / 2
+        canvas.drawCircle(widthHalf.toFloat(), heightHalf.toFloat(), widthHalf.toFloat(), paint)
+
+        paint.run {
+            color = Color.BLACK
+            xfermode = null
+        }
+        canvas.drawCircle(
+            width + (widthHalf - (profileMargin + profileOverlay)).toFloat(),
+            heightHalf.toFloat(),
+            widthHalf.toFloat(),
+            paint
+        )
+
+        return mask
+    }
+
+    private fun getClippedResource(resource: Bitmap, mask: Bitmap): Bitmap {
+        val result = Bitmap.createBitmap(resource.width, resource.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas()
+        canvas.setBitmap(result)
+        canvas.drawBitmap(resource, 0f, 0f, null)
+
+        val paint = Paint().apply {
+            isFilterBitmap = false
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+        }
+
+        canvas.drawBitmap(mask, 0f, 0f, paint)
+
+        return result
+    }
 }
+
+fun Context.dpToPx(dp: Int) = TypedValue.applyDimension(
+    TypedValue.COMPLEX_UNIT_DIP,
+    dp.toFloat(),
+    this.resources.displayMetrics
+).toInt()
