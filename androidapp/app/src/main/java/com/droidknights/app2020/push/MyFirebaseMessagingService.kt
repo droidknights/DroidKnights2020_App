d49f4b8df5f1b7596f8612d0d39e98360cf94051
jiyoung <ji9310@naver.com>
2020-09-02 21:53:29 +0900

    fcm token 서버에 저장

--- androidapp/app/src/main/java/com/droidknights/app2020/push/MyFirebaseMessagingService.kt
+++ androidapp/app/src/main/java/com/droidknights/app2020/push/MyFirebaseMessagingService.kt
@@ -1,21 +1,46 @@
 package com.droidknights.app2020.push
 
-import android.app.*
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.droidknights.app2020.R
 import com.droidknights.app2020.ui.MainActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    @Inject
+    lateinit var firestore: FirebaseFirestore
 
     override fun onNewToken(s: String) {
         super.onNewToken(s)
         Timber.d("FCM_NEW_TOKEN $s")
+
+        val data = hashMapOf(
+            "token" to s
+        )
+
+        firestore.collection("FcmToken")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                Log.d("MessagingService", "DocumentSnapshot written with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("MessagingService", "Error adding document", e)
+            }
+
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
